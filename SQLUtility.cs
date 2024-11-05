49eb40d9aef9592ec020ac9b1c0b7b96fb58b049
fbernstein <fcb613@gmail.com>
2024-11-04 22:02:53 -0500

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        internal static DataTable GetDataTable(string sqlstatement) //- take a SQL statement and return a DataTable
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = GetConnectionString();
+            conn.Open();
+            //DisplayMessage("Conn Status ", conn.State.ToString());
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
